144929782eccefa4baa992ef8f8b11594a0b7577
ruslan <aslanov.r.m@gmail.com>
2019-02-01 14:08:57 +0300

    minor changes

--- src/test/java/com/rasl/repository/UserAccountRepositoryImplTest.java
+++ src/test/java/com/rasl/repository/UserAccountRepositoryImplTest.java
@@ -25,26 +25,21 @@ public class UserAccountRepositoryImplTest {
                 "  middle_name varchar (50) not null,\n" +
                 "  last_name varchar (50)\n" +
                 ");";
-
-        @Language("PostgreSQL")
-        String createInserts =
-                "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login1', 'first_name1', 'middle_name1', 'last_name1');\n" +
-                "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login2', 'first_name2', 'middle_name2', 'last_name2');\n" +
-                "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login3', 'first_name3', 'middle_name3', 'last_name3');\n" +
-                "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login4', 'first_name4', 'middle_name4', 'last_name4');\n" +
-                "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login5', 'first_name5', 'middle_name5', 'last_name5');";
-
-        try{
-            Class.forName(DbCredentials.JDBC_DRIVER);
-        } catch (ClassNotFoundException e){
-            e.printStackTrace();
-        }
         try(Connection connection = DriverManager.getConnection(DbCredentials.URL, DbCredentials.USER, DbCredentials.PASSWORD);
             PreparedStatement preparedStatement = connection.prepareStatement(createTable)){
             preparedStatement.execute();
         } catch (SQLException e){
             e.printStackTrace();
         }
+
+        @Language("PostgreSQL")
+        String createInserts =
+                "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login1', 'first_name1', 'middle_name1', 'last_name1');\n" +
+                        "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login2', 'first_name2', 'middle_name2', 'last_name2');\n" +
+                        "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login3', 'first_name3', 'middle_name3', 'last_name3');\n" +
+                        "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login4', 'first_name4', 'middle_name4', 'last_name4');\n" +
+                        "INSERT INTO user_account (login, first_name, middle_name, last_name) VALUES ('login5', 'first_name5', 'middle_name5', 'last_name5');";
+
         try(Connection connection = DriverManager.getConnection(DbCredentials.URL, DbCredentials.USER, DbCredentials.PASSWORD);
             PreparedStatement preparedStatement = connection.prepareStatement(createInserts)){
             preparedStatement.execute();
